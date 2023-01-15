7052fe78295bb45a55ce4b5ac9228e5151c57ab6
Alef Silva <alefs93@gmail.com>
2023-01-15 16:19:24 -0300

    Adding Firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'core/route/router.dart';
 import 'core/theme/default_theme.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: App(),
