684ece09f511656ddb2719b852e0de2fa6b48313
Alef Silva <alefs93@gmail.com>
2023-01-15 15:51:34 -0300

    New neutral colors

--- lib/core/theme/colors/colors.dart
+++ lib/core/theme/colors/colors.dart
@@ -9,11 +9,13 @@ class AppColors {
   static const Color supportSuccess = Color.fromARGB(255, 69, 120, 109);
 
   static const Color neutral1 = Color(0xffffffff);
-  static const Color neutral50 = Color(0xffe7e9ee);
+  static const Color neutral25 = Color(0xffe7e9ee);
+  static const Color neutral50 = Color.fromARGB(255, 225, 228, 235);
   static const Color neutral100 = Color(0xffc1c7d7);
   static const Color neutral200 = Color(0xff9aa3bc);
   static const Color neutral300 = Color(0xffa0a3b8);
   static const Color neutral400 = Color(0xff74778b);
   static const Color neutral500 = Color(0xff606377);
+  static const Color neutral600 = Color.fromARGB(255, 66, 67, 77);
   static const Color neutral1000 = Color(0xff000000);
 }
