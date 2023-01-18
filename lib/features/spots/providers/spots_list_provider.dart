6929c5abd11c782ff09935cd8dff4778b8fc0112
Alef Silva <alefs93@gmail.com>
2023-01-18 12:09:41 -0300

    New avaliableSpotsListProvider

--- lib/features/spots/providers/spots_list_provider.dart
+++ lib/features/spots/providers/spots_list_provider.dart
@@ -1,13 +1,23 @@
 import 'dart:async';
 
+import 'package:ezpark/features/spots/enums/spot_status.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../core/network/response/entities/response_result.dart';
 import '../data/repositories/spot_repository_impl.dart';
 import '../domain/entities/spot.dart';
 import '../domain/repositories/spot_repository.dart';
 
 final spotsListProvider = AsyncNotifierProvider<SpotsListNotifier, List<Spot>>(
   () => SpotsListNotifier(),
+);
+
+final avaliableSpotsListProvider = FutureProvider<List<Spot>>(
+  (ref) async {
+    final repository = ref.read(spotRepositoryProvider);
+    final response = await repository.getSpots(spotStatus: SpotStatus.active);
+    return response.data!;
+  },
 );
 
 class SpotsListNotifier extends AsyncNotifier<List<Spot>> {
@@ -21,12 +31,16 @@ class SpotsListNotifier extends AsyncNotifier<List<Spot>> {
     return await getSpots();
   }
 
-  Future<void> refresh() async {
+  Future<void> refresh({
+    SpotStatus? spotStatus,
+  }) async {
     final response = await getSpots();
     state = AsyncValue.data(response);
   }
 
-  Future<List<Spot>> getSpots() async {
+  Future<List<Spot>> getSpots({
+    SpotStatus? spotStatus,
+  }) async {
     state = const AsyncValue.loading();
     final response = await _repository.getSpots();
     if (!response.success) {
@@ -47,5 +61,19 @@ class SpotsListNotifier extends AsyncNotifier<List<Spot>> {
             response.errorMessage!,
             StackTrace.current,
           );
+  }
+
+  Future<ResponseResult> setStatus({
+    required int spotNumber,
+    required SpotStatus spotStatus,
+  }) async {
+    final result = await _repository.setStatus(
+      spotNumber: spotNumber,
+      spotStatus: spotStatus,
+    );
+
+    ref.invalidate(avaliableSpotsListProvider);
+    ref.read(spotsListProvider.notifier).refresh();
+    return result;
   }
 }
