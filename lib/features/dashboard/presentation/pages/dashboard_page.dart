539a002090ef8c0a7141983be86185b58376177e
Alef Silva <alefs93@gmail.com>
2023-01-18 12:46:10 -0300

    New dashboard appearance

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -2,7 +2,6 @@ import 'package:ezpark/core/resposivity/extensions/resizer_extension.dart';
 import 'package:ezpark/core/sizes/font_size.dart';
 import 'package:ezpark/core/sizes/spacings.dart';
 import 'package:ezpark/core/theme/colors/colors.dart';
-import 'package:ezpark/core/theme/components/loading/loading_overlay.dart';
 import 'package:ezpark/features/spots/enums/spot_form_action.dart';
 import 'package:ezpark/features/spots/presentation/spot_dialog.dart';
 
@@ -16,7 +15,7 @@ class DashBoardPage extends StatelessWidget {
 
   final List<Widget> dashboardOption = const [
     _AddNewEntryCard(),
-    _SpotsCard(),
+    _EntriesListCard(),
     _RegisterNewSpotCard(),
     _ReportCard(),
   ];
@@ -29,16 +28,26 @@ class DashBoardPage extends StatelessWidget {
         elevation: 3,
         shadowColor: Colors.black,
       ),
-      body: LoadingOverlay(
-        child: Padding(
-          padding: const EdgeInsets.all(Spacings.xs),
-          child: GridView.builder(
-            itemCount: dashboardOption.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
+      body: Padding(
+        padding: const EdgeInsets.all(Spacings.xs),
+        child: Column(
+          children: [
+            const Flexible(
+              flex: 2,
+              child: _SpotsCard(),
             ),
-            itemBuilder: (context, index) => dashboardOption[index],
-          ),
+            Flexible(
+              flex: 5,
+              child: GridView.builder(
+                shrinkWrap: true,
+                itemCount: dashboardOption.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context, index) => dashboardOption[index],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -80,17 +89,47 @@ class _AddNewEntryCard extends StatelessWidget {
   }
 }
 
+class _EntriesListCard extends StatelessWidget {
+  const _EntriesListCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return _DashboardCard(
+        title: '',
+        onTap: () => context.push(Routes.entriesListPage.description),
+        child: Column(
+          children: [
+            Icon(
+              Icons.format_list_bulleted_outlined,
+              color: colorScheme.primary.withOpacity(.7),
+              size: 44.width,
+            ),
+            const SizedBox(
+              height: Spacings.xs,
+            ),
+            Text(
+              'Entradas',
+              textAlign: TextAlign.center,
+              style: textTheme.displayMedium!.copyWith(
+                color: colorScheme.primary,
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
 class _SpotsCard extends StatelessWidget {
   const _SpotsCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
     return _DashboardCard(
-      onTap: () => context.push(
-        Routes.spotsList.description,
-      ),
       title: 'Vagas',
       child: Column(
         children: [
@@ -132,16 +171,37 @@ class _SpotsCard extends StatelessWidget {
             ],
           ),
           SizedBox(
-            height: 10.height,
+            height: 18.height,
           ),
-          Text(
-            'Ver Vagas',
-            textAlign: TextAlign.center,
-            style: textTheme.displaySmall!.copyWith(
-              color: AppColors.neutral500,
-              decoration: TextDecoration.underline,
-              letterSpacing: 1,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 180.width,
+                child: OutlinedButton(
+                  onPressed: () => context.push(
+                    Routes.spotsList.description,
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    elevation: 3.height,
+                    shadowColor: Colors.black,
+                    backgroundColor: colorScheme.secondary,
+                    side: BorderSide(
+                      style: BorderStyle.solid,
+                      color: colorScheme.primary,
+                    ),
+                  ),
+                  child: Text(
+                    'Ver Vagas',
+                    textAlign: TextAlign.center,
+                    style: textTheme.displaySmall!.copyWith(
+                      color: AppColors.neutral500,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
