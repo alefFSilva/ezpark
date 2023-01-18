d7b1e431facfc0e8ff66ef4cde627caaf2771b2a
Alef Silva <alefs93@gmail.com>
2023-01-18 12:09:32 -0300

    Updating avaliable Spotslist

--- lib/features/spots/providers/save_spot_provider.dart
+++ lib/features/spots/providers/save_spot_provider.dart
@@ -1,4 +1,5 @@
 import 'package:ezpark/features/spots/enums/spot_form_action.dart';
+import 'package:ezpark/features/spots/providers/spots_list_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/network/response/entities/response_result.dart';
@@ -8,15 +9,18 @@ import '../domain/repositories/spot_repository.dart';
 
 final spotNotiferProvider =
     StateNotifierProvider<SaveSpotFormNotifier, AsyncValue<ResponseResult>>(
-  (ref) => SaveSpotFormNotifier(
-    spotRepository: ref.read(spotRepositoryProvider),
-  ),
+  ((ref) => SaveSpotFormNotifier(
+        spotRepository: ref.read(spotRepositoryProvider),
+        ref: ref,
+      )),
 );
 
 class SaveSpotFormNotifier extends StateNotifier<AsyncValue<ResponseResult>> {
   SaveSpotFormNotifier({
     required SpotRepository spotRepository,
+    required StateNotifierProviderRef ref,
   })  : _spotRepository = spotRepository,
+        _ref = ref,
         super(
           AsyncValue<ResponseResult>.data(
             ResponseResult.empty(),
@@ -24,18 +28,20 @@ class SaveSpotFormNotifier extends StateNotifier<AsyncValue<ResponseResult>> {
         );
 
   final SpotRepository _spotRepository;
+  final StateNotifierProviderRef _ref;
 
   Future<void> saveSpot(
     Spot spotToSave,
-    SpotFormAction spotFormAction,
+    RespositoryAction spotFormAction,
   ) async {
     state = const AsyncValue.loading();
 
-    final result = spotFormAction == SpotFormAction.add
+    final result = spotFormAction == RespositoryAction.add
         ? await _spotRepository.addSpot(spotToSave)
         : await _spotRepository.saveSpot(
             spotToSave: spotToSave,
           );
+    _ref.invalidate(avaliableSpotsListProvider);
 
     state = AsyncValue<ResponseResult<Spot>>.data(result);
   }
