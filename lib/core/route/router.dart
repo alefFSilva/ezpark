83530e053a88d0071282e5ce6aa44b802d4897ce
Alef Silva <alefs93@gmail.com>
2023-01-18 12:06:30 -0300

    Adding new newEntryPage route

--- lib/core/route/router.dart
+++ lib/core/route/router.dart
@@ -1,12 +1,14 @@
 import 'package:ezpark/features/dashboard/presentation/pages/dashboard_page.dart';
-import 'package:ezpark/features/spots/spots_list/pages/spots_list_page.dart';
+import 'package:ezpark/features/entry/presentation/pages/new_entry_page.dart';
+import 'package:ezpark/features/spots/presentation/pages/spots_list_page.dart';
 import 'package:go_router/go_router.dart';
 
 import '../initializer/widgets/initializer.dart';
 
 enum Routes {
   dashboard('/dashboard'),
-  spotsList('/spotslist');
+  spotsList('/spotslist'),
+  newEntryPage('/newEntryPage');
 
   const Routes(this.description);
   final String description;
@@ -17,15 +19,19 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const Initializer(),
+      builder: (_, __) => const Initializer(),
     ),
     GoRoute(
       path: Routes.dashboard.description,
-      builder: (context, state) => const DashBoardPage(),
+      builder: (_, __) => const DashBoardPage(),
     ),
     GoRoute(
       path: Routes.spotsList.description,
-      builder: (context, state) => const SpotsListPage(),
+      builder: (_, __) => const SpotsListPage(),
+    ),
+    GoRoute(
+      path: Routes.newEntryPage.description,
+      builder: (_, __) => const NewEntryPage(),
     )
   ],
 );
