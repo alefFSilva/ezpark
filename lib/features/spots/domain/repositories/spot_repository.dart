169db0469462dd9e48f52eb3b08347d90ab86de1
Alef Silva <alefs93@gmail.com>
2023-01-18 12:07:53 -0300

    SpotRepository - Adding status methods

--- lib/features/spots/domain/repositories/spot_repository.dart
+++ lib/features/spots/domain/repositories/spot_repository.dart
@@ -1,3 +1,5 @@
+import 'package:ezpark/features/spots/enums/spot_status.dart';
+
 import '../../../../../core/network/response/entities/response_result.dart';
 import '../entities/spot.dart';
 
@@ -6,8 +8,15 @@ abstract class SpotRepository {
   Future<ResponseResult<Spot>> saveSpot({
     required Spot spotToSave,
   });
-  Future<ResponseResult<List<Spot>>> getSpots();
+  Future<ResponseResult<List<Spot>>> getSpots({
+    SpotStatus? spotStatus,
+  });
   Future<ResponseResult> remove({
     required int spotNumber,
+  });
+
+  Future<ResponseResult> setStatus({
+    required int spotNumber,
+    required SpotStatus spotStatus,
   });
 }
